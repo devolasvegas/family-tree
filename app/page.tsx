9e383514ecaee2b6b0b79bac5e839eb364d91e63
Devon Daviau <devondaviau@yahoo.ca>
2025-11-13 15:14:24 -0500

    add a build flag to the package.json build script to try and keep query from running during build

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,12 @@ export default async function Home() {
       }
     }
   `;
-  const { data } = await query({ query: GET_PERSONS });
 
-  console.log(data);
+  if (!process.env.NEXT_BUILD) {
+    const { data } = await query({ query: GET_PERSONS });
+    console.log(data);
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
